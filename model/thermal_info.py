719744972325f1cac828b0bcc50f0c670f638587
sisimaimai <155883753+sisimaimai@users.noreply.github.com>
2024-01-28 23:16:58 +0900

    v2対応

--- model/thermal_info.py
+++ model/thermal_info.py
@@ -1,18 +1,15 @@
 from pydantic import BaseModel
-from pydantic import BaseModel, validator
 from typing import Optional
 
 from utils import logger
 
 LOGGER = logger.get_logger(__name__)
 
+
 class ThermalInfo(BaseModel):
     temperature: Optional[float]
     humidity: Optional[float]
 
-    @validator('temperature', 'humidity')
-    def validate_none(cls, value):
-        if value is None:
+    def validate(self):
+        if self.temperature is None or self.humidity is None:
             LOGGER.warning(f"Attribute is set to None.")
-        return value
-
