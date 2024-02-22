8b3dd044279275032d39cac38359670cee301db3
sisimaimai <155883753+sisimaimai@users.noreply.github.com>
2024-02-22 22:55:09 +0900

    環境変数をsettingsにまとめる

--- settings.py
+++ settings.py
@@ -4,3 +4,4 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     switchbot_token: str
     switchbot_secret: str
+    loglevel: str = "INFO"
--- utils/logger.py
+++ utils/logger.py
@@ -1,15 +1,17 @@
 import logging
 import os
 
-def get_logger(name: str) -> logging.Logger:
-    loglevel = os.environ.get("LOGLEVEL", logging.INFO)
+from settings import Settings
 
+SETTINGS = Settings()
+
+
+def get_logger(name: str) -> logging.Logger:
+    loglevel = SETTINGS.loglevel
     # 出力設定作成
     channel = logging.StreamHandler()
     channel.setLevel(loglevel)
-    channel.setFormatter(
-        logging.Formatter("%(levelname)s:%(name)s:%(message)s")
-    )
+    channel.setFormatter(logging.Formatter("%(levelname)s:%(name)s:%(message)s"))
 
     # ロガー作成
     logger = logging.getLogger(name)
